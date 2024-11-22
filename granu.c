e38f7901c9f3a6e7ff08c5fbfb206ef07e47a2d8
SollyMollyDolly <sollymollydolly@gmail.com>
2024-11-22 15:12:29 -0300

    added option to call quantity to print from command (-q)

--- granu.c
+++ granu.c
@@ -6,22 +6,28 @@
 #include <stdio.h>
 #include <time.h>
 
+#define MAX_LIMIT   0xFFFFFFFFFFFFFFFF
+#define NormalizeToShort(a) a %= 0x10000;
+#define NormalizeToInt(a)   a %= 0x100000000;
+
 int main(int argc, char **argv){
     char option;
     char separator = '\n';
     bool bigMax = false;
+    bool askQuantity = true;
     FILE *outputFile = stdout;
+    uint64_t upperBound = MAX_LIMIT;
     size_t quantity;
-    size_t upperBound = 0xFFFFFFFFFFFFFFFF;
 
-    while((option = getopt(argc, argv, "Mif:u:h?")) != -1){
+    while((option = getopt(argc, argv, "Miq:f:u:h?")) != -1){
         switch (option){
             case 'h':
                 printf("Options are:\n");
                 printf("\t-M:\t\tallows user to print up to %u numbers\n", ~0);
                 printf("\t-f [FILENAME]:\tprint numbers to [FILENAME]\n");
+                printf("\t-q [QUANTITY]:\tset amount of numbers to print\n");
                 printf("\t-i:\t\tprint numbers in a single line spaced by a single character\n");
-                printf("\t-u:\t\tset a max integer value\n");
+                printf("\t-u [MAX_VAL]:\tset a max integer value\n");
                 printf("\t-h:\t\tprint this message and exit\n");
                 return 0;
             case 'M':
@@ -30,6 +36,15 @@ int main(int argc, char **argv){
                 break;
             case 'i': 
                 separator = ' ';
+                break;
+            case 'q':
+                if (bigMax){ 
+                    printf("option -q disabled with large max enabled\n"); 
+                    break; 
+                }
+                sscanf(optarg, "%zu", &quantity);
+                askQuantity = false;
+                NormalizeToShort(quantity);
                 break;
             case 'f':
                 outputFile = fopen(optarg, "w");
@@ -50,39 +65,39 @@ int main(int argc, char **argv){
                 break;
         }
     }
+    if (askQuantity){
+        if (!bigMax) {
+            printf("Quantity of numbers to print (max %u): ", 0xFFFF);
+            scanf("%zu", &quantity);
+            NormalizeToShort(quantity);
+        }else {
+            printf("WARNING: Large limit enabled, be careful\n");
+            printf("Quantity of numbers to print (max %u): ", ~0);
+            scanf("%zu", &quantity);
+            NormalizeToInt(quantity);
+            if (quantity > 0xFFFF){
+            warning:
+                char key;
+                printf("WARNING: This will print a file of ~%zuMB\n", quantity / 100000);
+                printf("Are you REALLY sure you need %zu numbers? [Y/n] ", quantity);
+                getchar();
+                scanf("%c", &key);
+                switch (key){
+                    case 'y':
+                    case 'Y':
+                        break;
+                    case 'n':
+                    case 'N':
+                        return 1;
+                    default:
+                        goto warning;
+                        break;
 
-    if (bigMax){
-        printf("WARNING: Large limit enabled, be careful\n");
-        printf("Quantity of numbers to print (max %u): ", ~0);
-        scanf("%zu", &quantity);
-        quantity %= 0x100000000;
-        if (quantity > 0xFFFF){
-        warning:
-            char planC;
-            printf("WARNING: This will print a file of ~%zuMB\n", quantity / 100000);
-            printf("Are you REALLY sure you need %zu numbers? [Y/n] ", quantity);
-            getchar();
-            scanf("%c", &planC);
-            switch (planC){
-                case 'y':
-                case 'Y':
-                    break;
-                case 'n':
-                case 'N':
-                    return 1;
-                default:
-                    goto warning;
-                    break;
-
+                }
             }
         }
-    }else {
-        printf("Quantity of numbers to print (max %u): ", 0xFFFF);
-        scanf("%zu", &quantity);
-        quantity %= 0x10000;
+
     }
-
-
     srand(time(0));
 
     for (uint32_t i = 0; i < quantity; i++){
